b111881724bc35b211263d4ea3932786cd5f7cef
CHARLIE\Charlie <charlie881230@gmail.com>
2026-01-12 23:15:26 +0800

    dynamic package

--- src/main/java/com/demo/todolist/dto/AdminLoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.demo.todolist.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record AdminLoginRequest(
-        @NotBlank String username,
-        @NotBlank String password
-) {
-}
