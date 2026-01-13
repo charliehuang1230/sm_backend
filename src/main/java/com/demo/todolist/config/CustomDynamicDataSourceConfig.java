695e007e3ff8a1edc9101a6966d71f8ed74dd5d1
CHARLIE\Charlie <charlie881230@gmail.com>
2026-01-14 00:00:38 +0800

    Handle missing default datasource by using no-op DataSource to avoid startup connection

--- src/main/java/com/demo/todolist/config/CustomDynamicDataSourceConfig.java
+++ src/main/java/com/demo/todolist/config/CustomDynamicDataSourceConfig.java
@@ -8,8 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.jdbc.datasource.AbstractDataSource;
+import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,9 +24,24 @@ public class CustomDynamicDataSourceConfig {
 
     @Bean
     public DataSource defaultDataSource(DataSourceProperties properties) {
-        return properties.initializeDataSourceBuilder()
-                .type(HikariDataSource.class)
-                .build();
+        if (StringUtils.hasText(properties.getUrl())) {
+            return properties.initializeDataSourceBuilder()
+                    .type(HikariDataSource.class)
+                    .build();
+        }
+
+        // No-op datasource to avoid connecting during startup. Any accidental use will fail fast.
+        return new AbstractDataSource() {
+            @Override
+            public Connection getConnection() throws SQLException {
+                throw new SQLException("Default datasource not configured");
+            }
+
+            @Override
+            public Connection getConnection(String username, String password) throws SQLException {
+                return getConnection();
+            }
+        };
     }
 
     @Bean(name = "dataSource")
